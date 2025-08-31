e10e7e39894ace479bd5ebf85fe6cdb4629f2d06
RuipuCui <ruipucui@gmail.com>
2025-09-01 01:38:32 +1000

    set up app.js, redirect request to corresponding routes file

--- app.js
+++ app.js
@@ -12,4 +12,11 @@ app.get('/', (req, res) => {
   res.send('Vines backend is running');
 });
 
+app.use('/api/media', require('./routes/mediaRoutes'));
+// app.use('/api/gps', require('./routes/gpsRoutes'));
+// app.use('/api/scores', require('./routes/scoreRoutes'));
+// app.use('/api/metrics', require('./routes/metricsRoutes'));
+// app.use('/api/friends', require('./routes/friendRoutes'));
+// app.use('/api/users', require('./routes/userRoutes'));
+
 module.exports = app;
