ed41c69273dadf35e5fdf769ac6209c2be8aacc5
RuipuCui <ruipucui@gmail.com>
2025-09-10 16:30:55 +1000

    build daily score models

--- models/dailyScoreModel.js
+++ models/dailyScoreModel.js
@@ -1,1 +1,43 @@
 const pool = require('../config/db');
+
+const createDailyScore = async ({ userId, score_date, mental_health_score, mental_details}) => {
+  console.log("start uploading daily score");
+  const query = `
+    INSERT INTO daily_scores (user_id, score_date, mental_health_score, mental_details)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *;
+  `;
+  const values = [userId, score_date, mental_health_score, mental_details];
+  const res = await pool.query(query, values);
+  return res.rows[0];
+};
+
+const getDailyScoresByDate = async({userId, score_date}) => {
+    console.log("start get daily score by date");
+    let query = `
+        SELECT * FROM daily_scores
+        WHERE user_id = $1 AND score_date = $2
+    `;
+    const values = [userId, score_date];
+    const res = await pool.query(query, values);
+    return res.rows;
+};
+
+const getDailyScoresByDays = async({userId, days}) => {
+    console.log(`start get daily score from last ${days}`);
+    let query = `
+        SELECT * FROM daily_scores
+        WHERE user_id = $1
+        ORDER BY score_date DESC
+        LIMIT $2
+    `;
+    const values = [userId, days];
+    const res = await pool.query(query, values);
+    return res.rows;
+};
+
+module.exports= {
+    createDailyScore,
+    getDailyScoresByDate,
+    getDailyScoresByDays
+}
