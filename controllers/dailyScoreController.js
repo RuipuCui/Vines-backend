06345bc44c6c73e59d0991805785671799a4e730
RuipuCui <ruipucui@gmail.com>
2025-09-10 16:50:26 +1000

    implement getUserDailyScore function

--- controllers/dailyScoreController.js
+++ controllers/dailyScoreController.js
@@ -1,10 +1,27 @@
 const dailyScoreModel = require('../models/dailyScoreModel')
 
-const uploadUserDailyScore = async(req, res) => {
-
+const getUserDailyScore = async(req, res) => {
+    console.log(`get request upload user daily score`);
+    const { userId } = req.user;
+    const score_date = req.query?.score_date ?? null;
+    const days = req.query?.days ?? null;
+    try {
+        let getDailyScores;
+        if(score_date){
+            getDailyScores = await dailyScoreModel.getDailyScoresByDate(userId, score_date);
+        }else if(days){
+            getDailyScores = await dailyScoreModel.getDailyScoresByDays(userId, days);
+        }else{
+            return res.status(404).json({error: 'request paramater invalid'});
+        }
+        return res.status(200).json(getDailyScores);
+    } catch (err){
+        console.log("get daily score error ", err);
+        return res.status(500).json({ error: 'Failed to get daily score'});
+    }
 }
 
-const getUserDailyScore = async(req, res) => {
+const uploadUserDailyScore = async(req, res) => {
 
 }
 
