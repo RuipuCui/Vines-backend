46cb179e318ea6859ddfac9e9fa28950c35ad4e3
RuipuCui <ruipucui@gmail.com>
2025-11-03 16:04:44 +1100

    feature: enable CORS for API endpoints

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');  
+
+app.use(cors({
+  origin: '*',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+}));
+
+app.all(/.*/, (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 
 app.use(express.json());
 
